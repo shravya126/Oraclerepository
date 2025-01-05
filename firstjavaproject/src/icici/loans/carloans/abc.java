27e06445a7c5a165752a4d50f766b8530c02a6f4
shravya126 <shravyaraorao.244@gmail.com>
2025-01-05 16:19:28 +0530

    Added abc varibales in abc class from dev1 branch

--- firstjavaproject/src/icici/loans/carloans/abc.java
+++ firstjavaproject/src/icici/loans/carloans/abc.java
@@ -1,13 +1,12 @@
 package icici.loans.carloans;
 
 public class abc {
-
+	int a,b,c;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("krisha");
 		System.out.println("shravya");
 		System.out.println("kiran");
-		System.out.println("hi");
 	}
 	
 
